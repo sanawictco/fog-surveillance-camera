ec8bf4b1e358d519021ca561fcc4c1b9326550e4
majid kakavandi <majidkakavandi12776@gmail.com>
2025-09-23 14:23:51 +0330

    feat(validator): add class-validator and class-transformer

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import AppConfig from 'configs/app.config';
 import { setupSwaggerRegisteration } from './utilities/swaggerRegisteration';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if (AppConfig().environment !== 'production') {
     setupSwaggerRegisteration(app);
   }
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    }),
+  );
   await app.listen(AppConfig().port);
 }
 bootstrap();
